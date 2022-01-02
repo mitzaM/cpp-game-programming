91762b9d5a4040979e44b5990dc968230d6a5d56
Mihai Man <mitaman89@yahoo.com>
2022-01-02 16:04:37 +0200

    Move clouds

--- 01 Timber/01 Timber.cpp
+++ 01 Timber/01 Timber.cpp
@@ -20,7 +20,7 @@ int main() {
     Sprite spriteTree;
     spriteTree.setTexture(textureTree);
     spriteTree.setPosition(
-        (SCREEN_WIDTH - textureTree.getSize().x)/2, 0
+        (float)((SCREEN_WIDTH - textureTree.getSize().x)/2), 0
     );
 
     Texture textureBee;
@@ -44,6 +44,13 @@ int main() {
     spriteCloud3.setTexture(textureCloud);
     spriteCloud3.setPosition(0, 500);
 
+    bool cloud1Active = false;
+    bool cloud2Active = false;
+    bool cloud3Active = false;
+    float cloud1Speed = 0.0f;
+    float cloud2Speed = 0.0f;
+    float cloud3Speed = 0.0f;
+
     Clock clock;
 
     while (window.isOpen()) {
@@ -55,10 +62,10 @@ int main() {
 
         if (!beeActive) {
             srand((int)time(0));
-            beeSpeed = (rand() % 200) + 200;
+            beeSpeed = (float)(rand() % 200 + 200);
 
             srand((int)time(0) * 10);
-            float height = (rand() % 500) + 500;
+            float height = (float)(rand() % 500 + 500);
             spriteBee.setPosition(SCREEN_WIDTH + 200, height);
             beeActive = true;
         }
@@ -69,6 +76,63 @@ int main() {
             );
             if (spriteBee.getPosition().x < -100) {
                 beeActive = false;
+            }
+        }
+
+        if (!cloud1Active) {
+            srand((int)time(0) * 10);
+            cloud1Speed = (float)(rand() % 200);
+
+            srand((int)time(0) * 10);
+            float height = (float)(rand() % 150);
+            spriteCloud1.setPosition(-200, height);
+            cloud1Active = true;
+        }
+        else {
+            spriteCloud1.setPosition(
+                spriteCloud1.getPosition().x + (cloud1Speed * dt.asSeconds()),
+                spriteCloud1.getPosition().y
+            );
+            if (spriteCloud1.getPosition().x > SCREEN_WIDTH) {
+                cloud1Active = false;
+            }
+        }
+
+        if (!cloud2Active) {
+            srand((int)time(0) * 20);
+            cloud2Speed = (float)(rand() % 200);
+
+            srand((int)time(0) * 20);
+            float height = (float)(rand() % 300 - 150);
+            spriteCloud2.setPosition(-200, height);
+            cloud2Active = true;
+        }
+        else {
+            spriteCloud2.setPosition(
+                spriteCloud2.getPosition().x + (cloud2Speed * dt.asSeconds()),
+                spriteCloud2.getPosition().y
+            );
+            if (spriteCloud2.getPosition().x > SCREEN_WIDTH) {
+                cloud2Active = false;
+            }
+        }
+
+        if (!cloud3Active) {
+            srand((int)time(0) * 30);
+            cloud3Speed = (float)(rand() % 200);
+
+            srand((int)time(0) * 30);
+            float height = (float)(rand() % 450 - 150);
+            spriteCloud3.setPosition(-200, height);
+            cloud3Active = true;
+        }
+        else {
+            spriteCloud3.setPosition(
+                spriteCloud3.getPosition().x + (cloud3Speed * dt.asSeconds()),
+                spriteCloud3.getPosition().y
+            );
+            if (spriteCloud3.getPosition().x > SCREEN_WIDTH) {
+                cloud3Active = false;
             }
         }
 
