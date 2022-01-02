0d3e154dfb2830194b8058a6c7f8a4db030f9c3d
Mihai Man <mitaman89@yahoo.com>
2022-01-02 02:34:13 +0200

    Create and draw sprites

--- 01 Timber/01 Timber.cpp
+++ 01 Timber/01 Timber.cpp
@@ -1,20 +1,62 @@
-// 01 Timber.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include <SFML/Graphics.hpp>
 
-#include <iostream>
+using namespace sf;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+const int SCREEN_WIDTH = 1920;
+const int SCREEN_HEIGHT = 1080;
+
+int main() {
+    VideoMode vm(SCREEN_WIDTH, SCREEN_HEIGHT);
+    RenderWindow window(vm, "Timber!!!", Style::Fullscreen);
+
+    Texture textureBackground;
+    textureBackground.loadFromFile("graphics/background.png");
+    Sprite spriteBackground;
+    spriteBackground.setTexture(textureBackground);
+    spriteBackground.setPosition(0, 0);
+
+    Texture textureTree;
+    textureTree.loadFromFile("graphics/tree2.png");
+    Sprite spriteTree;
+    spriteTree.setTexture(textureTree);
+    spriteTree.setPosition(
+        (SCREEN_WIDTH - textureTree.getSize().x)/2, 0
+    );
+
+    Texture textureBee;
+    textureBee.loadFromFile("graphics/bee.png");
+    Sprite spriteBee;
+    spriteBee.setTexture(textureBee);
+    spriteBee.setPosition(0, 800);
+
+    Texture textureCloud;
+    textureCloud.loadFromFile("graphics/cloud.png");
+    Sprite spriteCloud1;
+    spriteCloud1.setTexture(textureCloud);
+    spriteCloud1.setPosition(0, 0);
+    Sprite spriteCloud2;
+    spriteCloud2.setTexture(textureCloud);
+    spriteCloud2.setPosition(0, 250);
+    Sprite spriteCloud3;
+    spriteCloud3.setTexture(textureCloud);
+    spriteCloud3.setPosition(0, 500);
+
+    while (window.isOpen()) {
+        if (Keyboard::isKeyPressed(Keyboard::Escape)) {
+            window.close();
+        }
+
+        window.clear();
+
+        window.draw(spriteBackground);
+        window.draw(spriteCloud1);
+        window.draw(spriteCloud2);
+        window.draw(spriteCloud3);
+        window.draw(spriteTree);
+        window.draw(spriteBee);
+
+        window.display();
+    }
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
