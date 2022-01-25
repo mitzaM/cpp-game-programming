294007617385b0779f7405392d2ebadf5c150ca0
Mihai Man <mitaman89@yahoo.com>
2022-01-25 23:55:05 +0200

    Initialize window and view

--- 03 Zombie Arena/03 Zombie Arena.cpp
+++ 03 Zombie Arena/03 Zombie Arena.cpp
@@ -1,6 +1,32 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
+#include "Player.h"
+
+using namespace sf;
 
 int main()
 {
+    enum class State { PAUSED, LEVELING_UP, GAME_OVER, PLAYING };
+    State state = State::GAME_OVER;
+
+    Vector2f resolution;
+    resolution.x = VideoMode::getDesktopMode().width;
+    resolution.y = VideoMode::getDesktopMode().height;
+
+    RenderWindow window(VideoMode(resolution.x, resolution.y), "Zombie Arena", Style::Fullscreen);
+    View mainView(sf::FloatRect(0, 0, resolution.x, resolution.y));
+
+    Clock clock;
+    Time gameTimeTotal;
+
+    Vector2f mouseWorldPosition;
+    Vector2i mouseScreenPosition;
+
+    Player player;
+    IntRect arena;
+
+    while (window.isOpen()) {
+
+    }
+
     return 0;
 }
