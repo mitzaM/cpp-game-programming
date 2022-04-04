ecfe971d92e018d491324dc6ffb4008070e2c735
Mihai Man <mitaman89@yahoo.com>
2022-04-04 22:59:39 +0300

    Code RectColliderComponent class

---
+++ 05 Space Invaders ++/RectColliderComponent.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <string>
+
+#include <SFML/Graphics.hpp>
+
+#include "ColliderComponent.h"
+
+
+class RectColliderComponent : public ColliderComponent
+{
+private:
+    std::string m_SpecificType = "rect";
+    sf::FloatRect m_Collider;
+    std::string m_Tag = "";
+
+public:
+    RectColliderComponent(std::string name);
+    std::string getColliderTag();
+    void setOrMoveCollider(float x, float y, float width, float height);
+    sf::FloatRect& getColliderRectF();
+
+    std::string getSpecificType()
+    {
+        return m_SpecificType;
+    }
+
+    void Component::start(GameObjectSharer* gos, GameObject* self) {};
+};
