bc8cf52c868a92ee773e500a52061061f8a3da58
Mihai Man <mitaman89@yahoo.com>
2022-07-07 16:53:34 +0300

    Initialize GameInputHandler class and add gamepad handling

--- 05 Space Invaders ++/GameInputHandler.cpp
+++ 05 Space Invaders ++/GameInputHandler.cpp
@@ -1,3 +1,5 @@
+#include <memory>
+
 #include "GameInputHandler.h"
 #include "GameScreen.h"
 #include "SoundEngine.h"
@@ -7,12 +9,39 @@ class BulletSpawner;
 
 void GameInputHandler::initialise()
 {
-
+    GameObject& player = getPointerToScreenManagerRemoteControl()->shareGameObjectSharer().findFirstObjectWithTag("Player");
+    m_PUC = std::static_pointer_cast<PlayerUpdateComponent>(player.getComponentByTypeAndSpecificType("update", "player"));
+    m_PTC = player.getTransformComponent();
 }
 
 void GameInputHandler::handleGamepad()
 {
+    float deadZone = 10.0f;
+    float x = sf::Joystick::getAxisPosition(0, sf::Joystick::X);
+    float y = sf::Joystick::getAxisPosition(0, sf::Joystick::Y);
 
+    if (x < deadZone && x > -deadZone) {
+        x = 0;
+    }
+    if (y < deadZone && y > -deadZone) {
+        y = 0;
+    }
+
+    m_PUC->updateShipTravelWithController(x, y);
+
+    if (sf::Joystick::isButtonPressed(0, 1)) {
+        m_BButtonPressed = true;
+    }
+
+    if (!sf::Joystick::isButtonPressed(0, 1) && m_BButtonPressed) {
+        m_BButtonPressed = false;
+
+        SoundEngine::playShoot();
+        sf::Vector2f spawnLocation;
+        spawnLocation.x = m_PTC->getLocation().x + m_PTC->getSize().x / 2;
+        spawnLocation.y = m_PTC->getLocation().y;
+        static_cast<GameScreen*>(getParentScreen())->getBulletSpawner()->spawnBullet(spawnLocation, true);
+    }
 }
 
 void GameInputHandler::handleKeyPressed(sf::Event& event, sf::RenderWindow& window)
@@ -60,5 +89,4 @@ void GameInputHandler::handleKeyReleased(sf::Event& event, sf::RenderWindow& win
             static_cast<GameScreen*>(getParentScreen())->spawnBullet(spawnLocation, true);
             break;
     }
-
 }
